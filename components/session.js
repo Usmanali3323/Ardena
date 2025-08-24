26ead5818defa30374a21e941181c58ebb7740de
Usmanali3323 <ua0318855@gmail.com>
2025-08-24 16:55:59 +0500

    second commit

--- components/session.js
+++ components/session.js
@@ -33,31 +33,31 @@ export default function useUser({ redirectToLogin = true } = {}) {
   return { user, load: loading };
 }
 
-export async function checkAdmin() {
-    const [admin, setAdmin] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const supabase = createClientComponentClient();
-  useEffect(()=>{
-    async  function isAdmin(){
-      const { data, error } = await supabase.auth.getUser();
-      if (error) {
-        console.error("Error fetching user:", error.message);
-      }
-      if(!data.user && redirectToLogin){
-        router.push("/login");
-      }
+// export async function checkAdmin() {
+//     const [admin, setAdmin] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const router = useRouter();
+//   const supabase = createClientComponentClient();
+//   useEffect(()=>{
+//     async  function isAdmin(){
+//       const { data, error } = await supabase.auth.getUser();
+//       if (error) {
+//         console.error("Error fetching user:", error.message);
+//       }
+//       if(!data.user && redirectToLogin){
+//         router.push("/login");
+//       }
 
-      if (data?.user.app_metadata.role !=="admin") {
-        router.push("/");
-      } else {
-        setUser(data?.user || null);
-      }
+//       if (data?.user.app_metadata.role !=="admin") {
+//         router.push("/");
+//       } else {
+//         setUser(data?.user || null);
+//       }
 
-      setLoading(false);
+//       setLoading(false);
             
-    }
-    isAdmin()
-      },[admin,load,redirectToLogin])
-      return  {admin,load}
-} 
+//     }
+//     isAdmin()
+//       },[admin,load,redirectToLogin])
+//       return  {admin,load}
+// } 
